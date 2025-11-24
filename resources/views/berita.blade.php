0be327ea34e92d28a2d4317ede0ee9ed1cf35bc2
haris2303 <hrsccf102@gmail.com>
2025-11-25 01:09:57 +0900

    feat(berita): tambah cards

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -3,4 +3,10 @@
     <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio sint ab assumenda sequi itaque laboriosam
         aspernatur vero eligendi nam pariatur doloribus delectus excepturi ullam architecto animi, dicta eum, nulla
         aliquid!</p>
+
+    <div class="cards">
+        <div class="card"></div>
+        <div class="card"></div>
+        <div class="card"></div>
+    </div>
 </x-web-layout>
