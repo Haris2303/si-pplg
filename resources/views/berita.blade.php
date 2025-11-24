98ead59ea2ef4f411ca069fb34d21a981bf32793
ahmadilham17 <hamzgithub@gmail.com>
2025-11-25 00:58:51 +0900

    feat: tambah paragraf di berita

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -1,3 +1,6 @@
 <x-web-layout>
     <h1>Berita</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio sint ab assumenda sequi itaque laboriosam
+        aspernatur vero eligendi nam pariatur doloribus delectus excepturi ullam architecto animi, dicta eum, nulla
+        aliquid!</p>
 </x-web-layout>
