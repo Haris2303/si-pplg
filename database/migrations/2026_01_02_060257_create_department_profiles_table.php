622d6f9dc6020e349e726c65690120dcc433101f
haris2303 <hrsccf102@gmail.com>
2026-01-02 15:33:34 +0900

    make:migration

---
+++ database/migrations/2026_01_02_060257_create_department_profiles_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('department_profiles', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null');
+            // background
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->string('image')->nullable();
+            $table->string('badge_text')->nullable();
+            $table->string('content');
+            $table->json('features')->nullable();
+            // vision & mision
+            $table->string('vision');
+            $table->string('mision');
+            // structure
+            $table->string('structure_image')->nullable();
+            // contact
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->json('social_media')->nullable();
+            $table->text('maps_embed')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('department_profiles');
+    }
+};
---
+++ database/migrations/2026_01_02_062757_create_teachers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('teachers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by')->nullable()->constrained('users')->onDelete('set null');
+            $table->string('name');
+            $table->string('image')->nullable();
+            $table->string('subject');
+            $table->json('social_media')->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('teachers');
+    }
+};
